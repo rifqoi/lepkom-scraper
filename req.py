f89b6a506830c7032e87da50446addf96b73b12a
rifqoi <alfurqon.rifqi@gmail.com>
2022-05-07 01:32:16 +0700

    session change to login session, add get_delete_praktikan

--- req.py
+++ req.py
@@ -9,7 +9,7 @@ class Scraper:
 
     def __init__(self, url):
         self.url = url
-        self.session = requests.Session()
+        self.session = self.login()
 
     def credentials(self, ):
         load_dotenv()
@@ -19,9 +19,10 @@ class Scraper:
 
         return USERNAME, PASSWORD
 
-    def login(self, ):
+    def login(self, ) -> requests.Session:
         username, password = self.credentials()
-        r = self.session.get(self.url)
+        session = requests.Session()
+        r = session.get(self.url)
         token_pattern = '<input type="hidden" name="logintoken" value="\w{32}">'
         token = re.findall(token_pattern, r.text)
         token = re.findall(r"\w{32}", token[0])
@@ -33,28 +34,47 @@ class Scraper:
             'logintoken': token[0]
         }
 
-        r = self.session.post(self.url, data=payload)
+        r = session.post(self.url, data=payload)
 
-        return self.session
+        return session
+
+    def get_courses(self) -> list:
+        r = self.session.get('https://kursusvmlepkom.gunadarma.ac.id/')
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        h3 = soup.find_all('h3', class_='coursename')
+        links = []
+        for link in h3:
+            a = link.find('a')
+            url = a.get('href')
+            text = a.text
+            links.append((url, text))
+
+        return links
+
+    def get_delete_praktikan(self, url) -> tuple:
+        r = self.session.get(url)
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+        r = soup.find_all('table')
+        tr = r[0].find_all('tr')
+        th = tr[0].find_all('th')
+        th = [t.text for t in th]
+
+        td_list = []
+
+        for row in tr[1:]:
+            td = row.find_all('td')
+            td = [t.text for t in td]
+            td_list.append(td)
+
+        return th, td_list
 
 
 def main():
 
     url = "https://kursusvmlepkom.gunadarma.ac.id/login/index.php"
     scraper = Scraper(url)
-    r = scraper.login()
-    r = r.get('https://kursusvmlepkom.gunadarma.ac.id/')
-    soup = BeautifulSoup(r.text, 'html.parser')
-    soup = soup.find_all("h3", class_="coursename")
-
-    links = []
-    for link in soup:
-        a = link.find('a')
-        url = a.get('href')
-        text = a.text
-        links.append((url, text))
-
-    print(links)
 
 
 if __name__ == "__main__":
